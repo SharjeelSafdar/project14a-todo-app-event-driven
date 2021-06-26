a63b2f41c7b047113d9c81a9d364fc057dba08b4
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-26 05:20:59 +0500

    Added dynamoDB table in backend-services-stack

--- backend/lib/p14a-backend-services-stack.ts
+++ backend/lib/p14a-backend-services-stack.ts
@@ -1,8 +1,29 @@
 import * as cdk from "@aws-cdk/core";
+import * as ddb from "@aws-cdk/aws-dynamodb";
 
 export class ServicesStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    /* ******************************************************** */
+    /* *************** DynamoDB Table for Todos *************** */
+    /* ******************************************************** */
+    const todosTable = new ddb.Table(this, "P14aTodosTable", {
+      partitionKey: {
+        name: "id",
+        type: ddb.AttributeType.STRING,
+      },
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    todosTable.addGlobalSecondaryIndex({
+      indexName: "username-index",
+      partitionKey: {
+        name: "username",
+        type: ddb.AttributeType.STRING,
+      },
+      projectionType: ddb.ProjectionType.ALL,
+    });
 
     cdk.Tags.of(this).add("Project", "P14a-Todo-App-event-driven");
   }
