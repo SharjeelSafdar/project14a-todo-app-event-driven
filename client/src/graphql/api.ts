d9a31080015878e0c63ef1e089439e4ada6b5bbc
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-27 04:54:27 +0500

    Updated Amplify config after redeploying the backend-services stack

--- client/src/graphql/api.ts
+++ client/src/graphql/api.ts
@@ -2,6 +2,19 @@
 /* eslint-disable */
 //  This file was automatically generated and should not be edited.
 
+export type TodoInput = {
+  id: string,
+  content: string,
+  status: boolean,
+};
+
+export type MutationCompletedReturnType = {
+  __typename: "MutationCompletedReturnType",
+  mutationType: string,
+  username: string,
+  todo: string,
+};
+
 export type Todo = {
   __typename: "Todo",
   id: string,
@@ -43,6 +56,21 @@ export type DeleteTodoMutation = {
   deleteTodo: string,
 };
 
+export type MutationCompletedMutationVariables = {
+  mutationType: string,
+  username: string,
+  todo: TodoInput,
+};
+
+export type MutationCompletedMutation = {
+  mutationCompleted:  {
+    __typename: "MutationCompletedReturnType",
+    mutationType: string,
+    username: string,
+    todo: string,
+  },
+};
+
 export type TodosQuery = {
   todos:  Array< {
     __typename: "Todo",
@@ -50,4 +78,17 @@ export type TodosQuery = {
     content: string,
     status: boolean,
   } >,
+};
+
+export type OnMutationCompletedSubscriptionVariables = {
+  username?: string | null,
+};
+
+export type OnMutationCompletedSubscription = {
+  onMutationCompleted?:  {
+    __typename: "MutationCompletedReturnType",
+    mutationType: string,
+    username: string,
+    todo: string,
+  } | null,
 };
---
+++ client/src/graphql/subscriptions.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onMutationCompleted = /* GraphQL */ `
+  subscription OnMutationCompleted($username: String) {
+    onMutationCompleted(username: $username) {
+      mutationType
+      username
+      todo
+    }
+  }
+`;
